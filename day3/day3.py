64e7a253f2d0256998208c99fec33bf1fdcda248
gabe <gabevenberg@gmail.com>
2022-08-20 22:35:27 -0500

    finished day 3

--- day3/day3.py
+++ day3/day3.py
@@ -8,12 +8,19 @@ from pprint import pprint
 
 def parse(puzzle_input: str):
     """Parse input"""
+    # returns a 2 dimentional array where true means there is a tree there.
+    return [[i=='#' for i in j] for j in puzzle_input.splitlines()]
 
 def part1(data):
     """Solve part 1"""
+    return solveForSlope(data, 3, 1)
+
+def solveForSlope(data, right: int, down: int):
+    return sum(1 for index, row in enumerate(data[::down]) if row[(right*index)%len(row)])
 
 def part2(data):
     """Solve part 2"""
+    return solveForSlope(data,1,1)*solveForSlope(data,3,1)*solveForSlope(data,5,1)*solveForSlope(data,7,1)*solveForSlope(data,1,2)
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input"""
